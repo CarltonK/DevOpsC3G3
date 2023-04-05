c8cef590ffd3e7d8c292b24f1452654de7140ace
Mark Carlton <mcarlton33@gmail.com>
2023-04-05 09:57:13 +0300

    docker-compose

--- Week-7/index.js
+++ Week-7/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/users');
+
+const connectionString = process.env.DATABASE_URL;
+
+mongoose.connect(connectionString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database connected');
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +25,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).json({ status: true });
 });
+
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
---
+++ Week-7/models/users.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userModel = new mongoose.Schema({
+    email: { required: true, type: String },
+    password: { required: true, type: String },
+    firstName: { required: false, type: String },
+    lastName: { required: false, type: String },
+});
+
+module.exports = mongoose.model('Users', userModel);
